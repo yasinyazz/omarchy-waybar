54f1976c6bc5c7aa5ec295ffa7f7e5fb4d7e3bc1
Muhammed Yasin M A <cr7yasincr7@gmail.com>
2026-04-29 12:55:25 +0530

    some tweaks

--- scripts/check-flatpak-updates.sh
+++ scripts/check-flatpak-updates.sh
@@ -7,5 +7,5 @@ COUNT=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
 if [ "$COUNT" -eq 0 ]; then
     echo '{"text": "", "tooltip": "Flatpaks are up to date", "class": "updated"}'
 else
-    echo "{\"text\": \"󰏓 $COUNT\", \"tooltip\": \"$COUNT flatpak update(s) available\", \"class\": \"outdated\"}"
+    echo "{\"text\": \" 󰏓 \", \"tooltip\": \"$COUNT flatpak update(s) available\", \"class\": \"outdated\"}"
 fi
